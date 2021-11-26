ec6d3f0481c4bdaafd1f086d727ad9cac8b62ef2
undina <foxundina@gmail.com>
2021-11-26 19:29:25 +0300

    HW1

--- src/test/java/ru/javawebinar/topjava/MatcherFactory.java
+++ src/test/java/ru/javawebinar/topjava/MatcherFactory.java
@@ -21,6 +21,10 @@ public class MatcherFactory {
         return new Matcher<>(clazz, fieldsToIgnore);
     }
 
+
+    public static <T> Matcher<T> usingEqualsComparator(Class<T> clazz) {
+        return new Matcher<>(clazz);
+    }
     public static class Matcher<T> {
         private final Class<T> clazz;
         private final String[] fieldsToIgnore;
